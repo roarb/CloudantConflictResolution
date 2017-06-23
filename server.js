2e78dab1d2f53fdb430d7035934f0a66d0a78a61
roarb <rhoehn24@gmail.com>
2017-06-23 08:47:00 -0500

    cron runtime update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var cron = require("node-cron");
 var childProcess = require('child_process'),
     ls;
 
-cron.schedule('* */1 * * *', function() {
+cron.schedule('0 */1 * * *', function() {
     ls = childProcess.exec('node index', function(err, stdout, stderr){
         // if (err){
         //     console.log(err);
